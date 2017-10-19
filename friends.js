c5b3f7b704552062d697f34799c5647368151e14
skylarpowers <skylarjpowers@gmail.com>
2017-10-19 08:47:41 -0700

    Add files via upload
    Final

---
+++ friends.js
@@ -0,0 +1,39 @@
+//Dummy Data
+
+var friends = [
+  {
+      "name":"Ben",
+      "photo":"https://cdn.stocksnap.io/img-thumbs/960w/DX0UF50OIM.jpg",
+      "scores":[
+         5,
+         1,
+         4,
+         4,
+         5,
+         1,
+         2,
+         5,
+         4,
+         1
+          ]
+      },
+  {
+  "name":"Cumorah",
+  "photo":"https://cdn.stocksnap.io/img-thumbs/960w/DX0UF50OIM.jpg",
+  "scores":[
+     5,
+     1,
+     2,
+     3,
+     5,
+     4,
+     1,
+     5,
+     4,
+     4
+      ]
+} 
+]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
